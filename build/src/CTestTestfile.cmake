984ec99776458640b4333f9f25ebc50c53ecdd46
lminzarl <alzayani.mansor@gmail.com>
2026-08-25 22:30:10 +0300

    removing the build dir

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/minz/purpose-uguu-upload/src
-# Build directory: /home/minz/purpose-uguu-upload/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
